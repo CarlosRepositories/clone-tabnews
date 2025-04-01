d9f18e85a29e44f1528a1196eddfb1f40f984190
ShaqRepo <70611136+CarlosRepositories@users.noreply.github.com>
2025-04-01 22:53:23 +0000

    alterando mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home(){
-    return <h1>Título correto</h1>
+    return <h1> 
+                Cara doutora Hadassa Coutinho, conforme 
+                eu tinha comentado antes, no feriado (de 18/04 a 21/04), vossa excelência está convidada
+                para dar um rolê comigo. Tá afim? (responde no zap kkk)
+           </h1>
 }
 
 export default Home;
